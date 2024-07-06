85fe607cfd03826dd49c1e4989d1eca499ae2a64
Douglas Moran <douglasmfdev@gmail.com>
2024-07-06 01:41:17 -0600

    feat(:sparkles:): set up ESLint, Prettier, and Husky to improve code quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb-typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    'react-native',
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'react-native/no-inline-styles': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
+
