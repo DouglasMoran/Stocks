13e30fe0b88159a4c9b13ef185465334c6379e7b
Douglas Moran <douglasmfdev@gmail.com>
2024-07-06 11:13:28 -0600

    build: :wrench: set up ts config

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,25 @@ module.exports = {
   plugins: [
     'react-native-reanimated/plugin',
     [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@interfaces': './src/interfaces',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    [
       'module:react-native-dotenv',
       {
         envName: 'APP_ENV',
